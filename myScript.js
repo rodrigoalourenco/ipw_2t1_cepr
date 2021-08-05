4987bb9e5d245d497ea067d37d0d8397c8686486
Rodrigo Lourenço <rodrigo.lourenco@blendit.com.br>
2021-08-04 22:21:22 -0300

    Hospedagem de site; Aplicando JavaScript ao HTML; Carregamento; Chrome DevTools; Operadores - Aritmético  - Atribuição  - Relacional  - Lógico.

---
+++ myScript.js
@@ -0,0 +1,19 @@
+console.log(5 + 6);
+document.getElementById('my-app').innerHTML = 5 + 6;
+
+
+// Variaveis
+var idade = 30;
+var preco = 25.56;
+var nome = 'Fulano';
+var acessoLiberado = true;
+
+idade = 'Trinta';
+
+// Operadores
+/*
+ - Aritmético: + - * / % Ex.: 5+6
+ - Atribuição: = += -= *= /= %= ++ -- Ex.: num = 10 / (num = num + 10 igual num += 10)
+ - Relacional: == != < <= > >= Ex.: 5 < 5 (false)
+ - Lógico: && || (5 == 5 && false) 
+ */
