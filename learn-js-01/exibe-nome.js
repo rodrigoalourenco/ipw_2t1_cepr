28f0913a0f8e080bbbe672fe1141d5d385b94ea3
Rodrigo Lourenço <rodrigo.lourenco@blendit.com.br>
2021-08-18 19:29:26 -0300

    Exibe Nome

---
+++ learn-js-01/exibe-nome.js
@@ -0,0 +1,9 @@
+for(var contador = 0; contador < 100; contador++) {
+    console.log('Rodrigo - ', contador);
+}
+
+var cont = 0;
+while (cont < 100) {
+    console.log('Rodrigo Lourenço - ' + cont);
+    cont++;
+}
